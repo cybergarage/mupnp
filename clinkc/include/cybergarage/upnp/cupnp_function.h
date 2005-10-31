a77e4439b42a8243196ccb03288215afde85853b
hjunnila <hjunnila@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2005-10-31 11:16:39 +0000

    Added comments to all functions and structs
    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/clinkc/trunk@26 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- clinkc/include/cybergarage/upnp/cupnp_function.h
+++ clinkc/include/cybergarage/upnp/cupnp_function.h
@@ -38,14 +38,42 @@ extern "C" {
 * Function
 ****************************************/
 
+/**
+ * Create a UUID (Universally Unique IDentifier) based on a semi-random value using current time.
+ *
+ * \param uuidBuf The UUID created by this function (must not be NULL)
+ * \return The UUID created by this function (== \e uuidBuf)
+ */
 char *cg_upnp_createuuid(char *uuidBuf);
+
+/**
+ * Get the server name ("OSname/OSversion/UPnP/<version> CyberLinkC/<version>")
+ *
+ * \param buf Buffer to store the server name
+ * \param bufSize Buffer length
+ * \param The server name buffer (== \e buf)
+ */
 char *cg_upnp_getservername(char *buf, int bufSize);
 
+/**
+ * Enable/disable NMPR (Intel Networked Media Product Requirements) mode.
+ *
+ * \param onFlag TRUE: enable; FALSE: disable
+ */
 void cg_upnp_setnmprmode(BOOL onFlag);
+
+/**
+ * Check whether NMPR (Intel Networked Media Product Requirements) mode is on.
+ * 
+ *
+ * \return TRUE if on; otherwise FALSE
+ */
 BOOL cg_upnp_isnmprmode();
 
 #ifdef  __cplusplus
-}
+  
+} /* extern "C" */
+
 #endif
 
 #endif
