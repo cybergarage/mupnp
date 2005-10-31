993676f3180ddd0d0112fedbb4891d3915fa5092
hjunnila <hjunnila@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2005-10-31 11:16:39 +0000

    Added comments to all functions and structs
    git-svn-id: https://svn.code.sf.net/p/clinkc/code/trunk@26 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- clinkc/include/cybergarage/upnp/cupnp_status.h
+++ clinkc/include/cybergarage/upnp/cupnp_status.h
@@ -11,6 +11,9 @@
 *	02/13/05
 *		- first revision
 *
+*	10/31/05
+*		- Added comments to all functions and structs
+*
 ******************************************************************/
 
 #ifndef _CG_UPNP_CUPNPSTATUS_H_
@@ -23,9 +26,9 @@
 extern "C" {
 #endif
 
-/****************************************
-* Define
-****************************************/
+/********************************************************************************
+ * Defines
+ ********************************************************************************/
 
 #define CG_UPNP_STATUS_INVALID_ACTION 401
 #define CG_UPNP_STATUS_INVALID_ARGS 402
@@ -34,31 +37,79 @@ extern "C" {
 #define CG_UPNP_STATUS_ACTION_FAILED 501
 
 /****************************************
-* Data Type
-****************************************/
+ * Data Type
+ ****************************************/
 
+/**
+ * UPnP status struct used in action and state operations
+ */
 typedef struct _CgUpnpStatus {
+	/** Status code */
 	int code;
+
+	/** Status description */
 	CgString *description;
 } CgUpnpStatus;
 
-/****************************************
-* Function
-****************************************/
+/******************************************************************************
+ * Function
+ ******************************************************************************/
 
+/**
+ * Create a new status struct
+ *
+ */
 CgUpnpStatus *cg_upnp_status_new();
+
+/**
+ * Delete a status struct
+ *
+ * \param upnpStat Status struct to delete
+ */
 void cg_upnp_status_delete(CgUpnpStatus *upnpStat);
 
+/**
+ * Set status code for status struct
+ *
+ * \param upnpStat Status struct
+ * \param value Status code
+ */
 #define cg_upnp_status_setcode(upnpStat, value) (upnpStat->code = value)
+
+/**
+ * Get Status code from a status struct
+ *
+ * \param upnpStat Status structure
+ */
 #define cg_upnp_status_getcode(upnpStat) (upnpStat->code)
 
+/**
+ * Set status description for a status struct
+ *
+ * \param upnpStat Status struct
+ * \param value Status description
+ */
 #define cg_upnp_status_setdescription(upnpStat, value) cg_string_setvalue(upnpStat->description, value)
+
+/**
+ * Get status description from a status struct
+ *
+ * \param upnpStat Status struct
+ */
 #define cg_upnp_status_getdescription(upnpStat) cg_string_getvalue(upnpStat->description)
 
+/**
+ * Helper function that converts a status code into a status description
+ *
+ * \param code Code to convert into a description
+ * \return Status description
+ */
 char *cg_upnp_status_code2string(int code);
 
 #ifdef  __cplusplus
-}
+
+} /* extern "C" */
+
 #endif
 
 #endif
