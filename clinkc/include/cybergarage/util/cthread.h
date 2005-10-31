6994b8779f304ce0a0910a2019e57d280f2ed433
hjunnila <hjunnila@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2005-10-31 11:48:16 +0000

    Added comments to all functions and structs
    git-svn-id: https://svn.code.sf.net/p/clinkc/code/trunk@28 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- clinkc/include/cybergarage/util/cthread.h
+++ clinkc/include/cybergarage/util/cthread.h
@@ -11,6 +11,9 @@
 *	02/07/05
 *		- first revision
 *
+*	10/31/05
+*		- Added comments to all functions and structs
+*
 ******************************************************************/
 
 #ifndef _CG_UTIL_CTHREAD_H_
@@ -30,6 +33,7 @@
 #include <btron/proctask.h>
 #else
 #include <pthread.h>
+#include <signal.h>
 #endif
 
 #ifdef  __cplusplus
@@ -45,7 +49,9 @@ extern "C" {
 ****************************************/
 
 typedef struct _CgThread {
+	/** Indicates whether the thread is running or not */
 	BOOL runnableFlag;
+
 #if defined(WIN32) && !defined(ITRON)
 	HANDLE	hThread;
 	DWORD	threadID;
@@ -58,30 +64,96 @@ typedef struct _CgThread {
 #elif defined(TENGINE) && defined(PROCESS_BASE)
 	WERR taskID;
 #else
-	pthread_t pThread;
-#endif
-	void (*action)(struct _CgThread *);
-	void *userData;
-} CgThread;
 
+  /** The POSIX thread handle */
+  pthread_t pThread;
+
+#endif
+
+  /** Thread's worker function */
+  void (*action)(struct _CgThread *);
+
+  /** Arbitrary user data pointer */
+  void *userData;
+
+} CgThread, CgThreadList;
+
+/**
+ * Prototype for the threads' worker functions 
+ */
 typedef void (*CG_THREAD_FUNC)(CgThread *);
 
 /****************************************
 * Function
 ****************************************/
 
+/**
+ * Create a new thread
+ */
 CgThread *cg_thread_new();
+
+/**
+ * Stop and destroy a thread.
+ *
+ * \param thread Thread to destroy
+ */
 BOOL cg_thread_delete(CgThread *thread);
+
+/**
+ * Start a thread (must be created first with ch_thread_new())
+ *
+ * \param thread Thread to start
+ */
 BOOL cg_thread_start(CgThread *thread);
+
+/**
+ * Stop a running thread.
+ *
+ * \param thread Thread to stop
+ */
 BOOL cg_thread_stop(CgThread *thread);
+
+/**
+ * Restart a thread. Essentially calls cg_thread_stop() and cg_thread_start()
+ *
+ * \param thread Thread to restart
+ */
 BOOL cg_thread_restart(CgThread *thread);
+
+/**
+ * Check if a thread has been started
+ *
+ * \param thread Thread to check
+ */
 BOOL cg_thread_isrunnable(CgThread *thread);
+
+/**
+ * Set the thread's worker function.
+ *
+ * \param thread Thread struct
+ * \param actionFunc Function pointer to set as the worker function
+ */
 void cg_thread_setaction(CgThread *thread, CG_THREAD_FUNC actionFunc);
+
+/**
+ * Set the user data pointer
+ *
+ * \param thread Thread struct
+ * \param data Pointer to user data
+ */
 void cg_thread_setuserdata(CgThread *thread, void *data);
+
+/**
+ * Get the user data pointer
+ *
+ * \param thread Thread from which to get the pointer
+ */
 void *cg_thread_getuserdata(CgThread *thread);
 
 #ifdef  __cplusplus
-}
+
+} /* extern "C" */
+
 #endif
 
 #endif
