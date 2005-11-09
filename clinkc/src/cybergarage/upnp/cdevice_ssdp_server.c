6ee8c832c34bca1131caac808ffb26ede675c3de
isimpson <isimpson@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2005-11-09 14:13:59 +0000

    Now checks validity of request line and all headers (HOST, MAN, MX) in function cg_upnp_device_ssdpmessagereceived
    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/clinkc/trunk@68 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- clinkc/src/cybergarage/upnp/cdevice_ssdp_server.c
+++ clinkc/src/cybergarage/upnp/cdevice_ssdp_server.c
@@ -36,25 +36,61 @@ void cg_upnp_device_ssdpmessagereceived(CgUpnpDevice *dev, CgUpnpSSDPPacket *ssd
 	CgUpnpService *service;
 	CgUpnpDevice *childDev;
 	
+	/****************************************
+	 * Request line
+	 * Check the request line for errors, this is not ideal as it currently only
+	 * checks for the presence of the strings and not the order.
+	 ***************************************/
+	/**** check for M-SEARCH and return if not found ****/
+	if (cg_strstr(cg_string_getvalue(ssdpPkt->dgmPkt->data), CG_HTTP_MSEARCH) < 0)
+		return;
+	/**** check for * and return if not found ****/
+	if (cg_strstr(cg_string_getvalue(ssdpPkt->dgmPkt->data), "*") < 0)
+		return;
+	/**** check HTTP version and return if not found ****/
+	if (cg_strstr(cg_string_getvalue(ssdpPkt->dgmPkt->data),  CG_HTTP_VER11) < 0)
+		return;
+
+	/****************************************
+	 * check HOST header, should always be 239.255.255.250:1900, return if incorrect
+	 ***************************************/
+	if (cg_strcmp(cg_upnp_ssdp_packet_gethost(ssdpPkt), CG_UPNP_SSDP_MULTICAST_ADDRESS) != 0)
+		return;
+
+	/****************************************
+	 * check MAN header, return if incorrect
+	 ***************************************/
+	if (cg_upnp_ssdp_packet_isdiscover(ssdpPkt) == FALSE)
+		return;
+
+	/****************************************
+	 * check MX header, return if incorrect
+	 ***************************************/
+	if (cg_http_headerlist_getvalue(ssdpPkt->headerList, CG_HTTP_MX) == NULL)
+		return;
+
+	/****************************************
+	 * check ST header and if empty return
+	 ***************************************/
 	ssdpST = cg_upnp_ssdp_packet_getst(ssdpPkt);
 	if (cg_strlen(ssdpST) <= 0)
 		return;
-		
+
 	isRootDev = cg_upnp_device_isrootdevice(dev);
 	
 	if (cg_upnp_st_isalldevice(ssdpST) == TRUE) {
-		// for root device only
+		/* for root device only */
 		if (isRootDev == TRUE) {
 			cg_upnp_device_getnotifydevicent(dev, ssdpMsg, sizeof(ssdpMsg));
 			cg_upnp_device_getnotifydeviceusn(dev, deviceUSN, sizeof(deviceUSN));
 			cg_upnp_device_postsearchresponse(dev, ssdpPkt, ssdpMsg, deviceUSN);
 		}
-		// for all devices send
-		// device type : device version
+		/* for all devices send */
+		/* device type : device version */
 		cg_upnp_device_getnotifydevicetypent(dev, ssdpMsg, sizeof(ssdpMsg));
 		cg_upnp_device_getnotifydevicetypeusn(dev, deviceUSN, sizeof(deviceUSN));
 		cg_upnp_device_postsearchresponse(dev, ssdpPkt, ssdpMsg, deviceUSN);
-		// device UUID
+		/* device UUID */
 		cg_upnp_device_postsearchresponse(dev, ssdpPkt, cg_upnp_device_getudn(dev), cg_upnp_device_getudn(dev));
 	}
 	else if (cg_upnp_st_isrootdevice(ssdpST)  == TRUE) {
