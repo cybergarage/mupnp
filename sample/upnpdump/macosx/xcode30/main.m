ee5fffca4d0608894971252d00bda8be31b839f4
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2008-04-16 13:59:37 +0000

    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/clinkc/trunk@259 bfd3be0d-9f21-0410-925f-e30e9767a5cc

---
+++ sample/upnpdump/macosx/xcode30/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  upnpdump
+//
+//  Created by ?? ? on 08/04/16.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
