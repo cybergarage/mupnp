fa6495dad16c7edc8e2d14df39b90743ee3a2e76
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2008-04-17 13:16:32 +0000

    git-svn-id: https://svn.code.sf.net/p/clinkc/code/trunk@260 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- wrapper/objc/CyberGarage/UPnP/CGControlPoint.h
+++ wrapper/objc/CyberGarage/UPnP/CGControlPoint.h
@@ -3,14 +3,14 @@
 //  clinkc
 //
 //  Created by Satoshi Konno on 08/03/14.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//  Copyright 2008 Satoshi Konno. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
 #include <cybergarage/upnp/ccontrolpoint.h>
 
-@interface ControlPoint : NSObject {
+@interface CGControlPoint : NSObject {
 	CgUpnpControlPoint *cgCtrlPoint;
 
 }
