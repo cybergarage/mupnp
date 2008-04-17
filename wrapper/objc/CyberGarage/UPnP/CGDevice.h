66b60b3da50bee7615d90922b293b0cf470ba946
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2008-04-17 16:42:12 +0000

    git-svn-id: https://svn.code.sf.net/p/clinkc/code/trunk@261 bfd3be0d-9f21-0410-925f-e30e9767a5cc

---
+++ wrapper/objc/CyberGarage/UPnP/CGDevice.h
@@ -0,0 +1,18 @@
+//
+//  ControlPoint.h
+//  clinkc
+//
+//  Created by Satoshi Konno on 08/03/14.
+//  Copyright 2008 Satoshi Konno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#include <cybergarage/upnp/cdevice.h>
+
+@interface CGDevice : NSObject {
+	CgUpnpDevice *cgDev;
+
+}
+
+@end
