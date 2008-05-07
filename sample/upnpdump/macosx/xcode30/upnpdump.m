8a64ad7b34a2eadb060f902c66ab20c00e1d99c4
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2008-05-07 17:46:38 +0000

    Updated project files for MacOSX
    git-svn-id: https://svn.code.sf.net/p/clinkc/code/trunk@272 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- sample/upnpdump/macosx/xcode30/upnpdump.m
+++ sample/upnpdump/macosx/xcode30/upnpdump.m
@@ -1,22 +1,14 @@
 #import <Foundation/Foundation.h>
-#import <CyberGarage/UPnP/CGUpnpControlPoint.h>
+#import <CyberGarage/UPnP/CGUpnp.h>
 
 int main (int argc, const char * argv[])
 {
-/*
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, World!");
-    [pool drain];
-*/
 	CGUpnpControlPoint *ctrlPoint = [[CGUpnpControlPoint alloc] init];
 	[ctrlPoint search];
 	sleep(5);
 	NSArray *devArray = [ctrlPoint getDeviceArray];
-	int i;
-	for (i=0; i < [devArray count]; i++) {
-		printf("%d¥n", i);
+	for (CGUpnpDevice *dev in devArray) {
+		NSLog(@"%@", [dev friendlyName]);
 	}
 	return 0;
 }
---
+++ wrapper/objc/CyberGarage/UPnP/CGUpnp.h
@@ -0,0 +1,11 @@
+//
+//  CGUpnp.h
+//  CyberLink for C
+//
+//  Created by Satoshi Konno on 08/05/08.
+//  Copyright 2008 Satoshi Konno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <CyberGarage/UPnP/CGUpnpControlPoint.h>
+#import <CyberGarage/UPnP/CGUpnpDevice.h>
