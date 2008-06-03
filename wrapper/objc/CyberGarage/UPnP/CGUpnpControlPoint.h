9f407bfccb347ec2b24e00e91c0aa407f26b4001
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2008-06-03 19:23:40 +0000

    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/clinkc/trunk@322 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- wrapper/objc/CyberGarage/UPnP/CGUpnpControlPoint.h
+++ wrapper/objc/CyberGarage/UPnP/CGUpnpControlPoint.h
@@ -18,9 +18,26 @@ typedef void CgUpnpControlPoint;
 }
 @property(readonly) CgUpnpControlPoint *cObject;
 - (CgUpnpControlPoint *)cObject;
+/**
+ * Activate the control point. Starts listening for SSDP messages etc.
+ * You must call this function before you can actually use a control point.
+ *
+ * @param ctrlPoint The control point to start
+ *
+ * @return TRUE if successful; otherwise FALSE
+ *
+ */
 - (BOOL)start;
 - (BOOL)stop;
+/**
+ * Do an M-SEARCH to look for all devices using ssdp:all in the network.
+ */
 - (void)search;
+/**
+ * Do an M-SEARCH to look for devices in the network.
+ *
+ * @param aST The Search Target parameter (ex. "ssdp:all")
+ */
 - (void)searchWithST:(NSString *)aST;
 - (NSArray *)devices;
 @end
