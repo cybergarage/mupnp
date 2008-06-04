d18bc36b1bd0f8c3cf2a84b607a56a110ab2ce22
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2008-06-04 20:55:54 +0000

    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/clinkc/trunk@326 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- wrapper/objc/CyberGarage/UPnP/CGUpnpDevice.h
+++ wrapper/objc/CyberGarage/UPnP/CGUpnpDevice.h
@@ -14,16 +14,55 @@ typedef void CgUpnpDevice;
 
 @class CGUpnpService;
 
+/**
+ * The CGUpnpDevice class is a wrapper class for CgUpnpDevice of CyberLink for C.
+ */
 @interface CGUpnpDevice : NSObject 
 {
 	CgUpnpDevice *cObject;
 }
 @property(readonly) CgUpnpDevice *cObject;
 - (id) initWithCObject:(CgUpnpDevice *)cobj;
+/**
+ * Get the friendly name of the device.
+ * 
+ * @return Friendly name.
+ */
 - (NSString *)friendlyName;
+/**
+ * Get the device type of the device.
+ * 
+ * @return Device Type.
+ */
 - (NSString *)deviceType;
+/**
+ * Check whether the device type is the specified type.
+ *
+ * @param type A type string of the device.
+ * 
+ * @return YES if the specified type is same as the device type; otherwise NO.
+ */
 - (BOOL)isDeviceType:(NSString *)type;
+/**
+ * Get all services in the device as a NSArray object. The array has the services as instances of CGUpnpService.
+ *
+ * @return NSArray of CGUpnpService.
+ */
 - (NSArray *)services;
-- (CGUpnpService *)getServiceByID:(NSString *)serviceId;
-- (CGUpnpService *)getServiceByType:(NSString *)serviceType;
+/**
+ * Get a service in the device by the specified service ID.
+ *
+ * @param name A ID string of the service.
+ *
+ * @return The CGUpnpService if the specified service is found; otherwise nil.
+ */
+- (CGUpnpService *)getServiceForID:(NSString *)serviceId;
+/**
+ * Get a service in the device by the specified service type.
+ *
+ * @param name A type string of the service.
+ *
+ * @return The CGUpnpService if the specified service is found; otherwise nil.
+ */
+- (CGUpnpService *)getServiceForType:(NSString *)serviceType;
 @end
