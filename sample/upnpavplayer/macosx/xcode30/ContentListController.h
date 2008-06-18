745df69af000cd95d69ad6efb6b2869456b9e4ec
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2008-06-18 17:52:12 +0000

    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/clinkc/trunk@337 bfd3be0d-9f21-0410-925f-e30e9767a5cc

---
+++ sample/upnpavplayer/macosx/xcode30/ContentListController.h
@@ -0,0 +1,16 @@
+//
+//  ContentListController.h
+//  upnpavplayer
+//
+//  Created by 今野 賢 on 08/06/19.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ContentListController : NSObject {
+
+}
+
+@end
