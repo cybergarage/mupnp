a6c8ceecda3e0278f9dbd1c1b1ed9f019c478a8e
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2008-07-18 13:47:21 +0000

    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/clinkc/trunk@367 bfd3be0d-9f21-0410-925f-e30e9767a5cc

---
+++ testunit/macosx/xcode30/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ClinkTestUnit
+//
+//  Created by ?? ? on 08/07/18.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
