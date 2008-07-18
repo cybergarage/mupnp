1b0b37247a6e23a5da4faddada3c9611fd9c448e
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2008-07-18 13:47:21 +0000

    git-svn-id: https://svn.code.sf.net/p/clinkc/code/trunk@367 bfd3be0d-9f21-0410-925f-e30e9767a5cc

---
+++ testunit/macosx/xcode30/ClinkUnitTest.h
@@ -0,0 +1,14 @@
+//
+//  ClinkUnitTest.h
+//  ClinkUnitTest
+//
+//  Created by Satoshi Konno on 08/07/15.
+//  Copyright 2008 CyberGarage. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface ClinkUnitTest : SenTestCase {
+}
+@end
