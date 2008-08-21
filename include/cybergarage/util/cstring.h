d2ca65e399df2bf502c1eede024f8b0b75db79c1
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2008-08-21 13:23:09 +0000

    * Added the following macros.   cg_string_getintvalue()   cg_string_getlongvalue()   cg_xml_node_getintvalue()   cg_xml_node_getlongvalue()   cg_xml_node_getchildnodeintvalue()   cg_xml_node_getchildnodelongvalue() * Changed the following functions to return an integer value instead of a string.   cg_upnp_icon_getwidth()   cg_upnp_icon_getheight()   cg_upnp_icon_getdepth()
    git-svn-id: https://svn.code.sf.net/p/clinkc/code/trunk@403 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- include/cybergarage/util/cstring.h
+++ include/cybergarage/util/cstring.h
@@ -46,57 +46,11 @@ extern "C" {
 
  /*  ULLONG_MAX : 18446744073709551615ULL */
 #define CG_STRING_LONGLONG_BUFLEN 32
-
-/****************************************
-* Data Type
-****************************************/
-
-typedef struct _CgString {
-	char *value;
-	int memSize;
-	int valueSize;
-} CgString;
-
-typedef struct _CgStringTokenizer {
-	char *value;
-	char *delim;
-	int delimCnt;
-	int nextStartPos;
-	int lastPos;
-	char *currToken;
-	char *nextToken;
-	char repToken;
-	BOOL hasNextTokens;
-} CgStringTokenizer;
-
-/****************************************
-* Function (String)
-****************************************/
-
-CgString *cg_string_new();
-void cg_string_delete(CgString *str);
-void cg_string_clear(CgString *str);
-void cg_string_setvalue(CgString *str, char *value);
-void cg_string_setintvalue(CgString *str, int value);
-void cg_string_setlongvalue(CgString *str, long value);
-void cg_string_setnvalue(CgString *str, char *value, int len);
-void cg_string_setpointervalue(CgString *str, char *value, int len);
-char *cg_string_getvalue(CgString *str);
-int cg_string_length(CgString *str);
-
-char *cg_string_addvalue(CgString *str, char *value);
-char *cg_string_naddvalue(CgString *str, char *value, int valueLen);
-char *cg_string_addrepvalue(CgString *str, char *value, int repeatCnt);
-char *cg_string_naddrepvalue(CgString *str, char *value, int valueLen, int repeatCnt);
-
-char *cg_string_replace(CgString *str, char *fromStr[], char *toStr[], int fromStrCnt);
-
-int cg_string_getmemorysize(CgString *str);
-
+	
 /****************************************
 * Function 
 ****************************************/
-
+	
 char *cg_strdup(char *str);
 int cg_strlen(char *str);
 char *cg_strcpy(char *dest, char *src);
@@ -117,16 +71,16 @@ char *cg_int2str(int value, char *buf, int bufSize);
 char *cg_long2str(long value, char *buf, int bufSize);
 char *cg_strncpy(char *str1, char *str2, size_t cnt);
 char *cg_strncat(char *str1, char *str2, size_t cnt);
-
+	
 #if defined(CG_USE_INT64)
 char *cg_longlong2str(CgInt64 value, char *buf, int bufSize);
 #endif
-
+	
 #define cg_str2int(value) (value ? atoi(value) : 0)
 #define cg_str2long(value) (value ? atol(value) : 0)
 #define cg_strhex2long(value) (value ? strtol(value, NULL, 16) : 0)
 #define cg_strhex2ulong(value) (value ? strtoul(value, NULL, 16) : 0)
-
+	
 #if defined(CG_USE_INT64)
 #if defined(__USE_ISOC99) || defined(HAVE_LONGLONG)
 #define cg_str2longlong(value) (value ? atoll(value) : 0)
@@ -138,16 +92,68 @@ char *cg_longlong2str(CgInt64 value, char *buf, int bufSize);
 #endif
 
 /****************************************
+* Data Type
+****************************************/
+	
+typedef struct _CgString {
+	char *value;
+	int memSize;
+	int valueSize;
+} CgString;
+	
+typedef struct _CgStringTokenizer {
+	char *value;
+	char *delim;
+	int delimCnt;
+	int nextStartPos;
+	int lastPos;
+	char *currToken;
+	char *nextToken;
+	char repToken;
+	BOOL hasNextTokens;
+} CgStringTokenizer;
+	
+	
+/****************************************
 * Function (StringTokenizer)
 ****************************************/
-
+	
 CgStringTokenizer *cg_string_tokenizer_new(char *str, char *delim);
 void cg_string_tokenizer_delete(CgStringTokenizer *strToken);
 BOOL cg_string_tokenizer_hasmoretoken(CgStringTokenizer *strToken);
 char *cg_string_tokenizer_nexttoken(CgStringTokenizer *strToken);
 char *cg_string_tokenizer_nextalltoken(CgStringTokenizer *strToken);
-
+	
 void cg_string_tokenizer_print(CgStringTokenizer *strToken);
+	
+/****************************************
+* Function (String)
+****************************************/
+
+CgString *cg_string_new();
+void cg_string_delete(CgString *str);
+void cg_string_clear(CgString *str);
+
+void cg_string_setvalue(CgString *str, char *value);
+void cg_string_setintvalue(CgString *str, int value);
+void cg_string_setlongvalue(CgString *str, long value);
+void cg_string_setnvalue(CgString *str, char *value, int len);
+void cg_string_setpointervalue(CgString *str, char *value, int len);
+
+char *cg_string_getvalue(CgString *str);
+#define cg_string_getintvalue(str) cg_str2int(cg_string_getvalue(str))
+#define cg_string_getlongvalue(str) cg_str2long(cg_string_getvalue(str))
+	
+int cg_string_length(CgString *str);
+
+char *cg_string_addvalue(CgString *str, char *value);
+char *cg_string_naddvalue(CgString *str, char *value, int valueLen);
+char *cg_string_addrepvalue(CgString *str, char *value, int repeatCnt);
+char *cg_string_naddrepvalue(CgString *str, char *value, int valueLen, int repeatCnt);
+
+char *cg_string_replace(CgString *str, char *fromStr[], char *toStr[], int fromStrCnt);
+
+int cg_string_getmemorysize(CgString *str);
 
 #ifdef  __cplusplus
 }
