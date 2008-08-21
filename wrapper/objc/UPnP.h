5a793b6773486da21f5e08462356911b3045412b
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2008-08-21 02:50:56 +0000

    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/clinkc/trunk@398 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- wrapper/objc/UPnP.h
+++ wrapper/objc/UPnP.h
@@ -6,16 +6,9 @@
 //  Copyright 2008 Satoshi Konno. All rights reserved.
 //
 
-#if !defined(CG_NOUSE_CLINKC_FRAMEWORK)
 #import <CyberLink/CGUpnpControlPoint.h>
 #import <CyberLink/CGUpnpDevice.h>
 #import <CyberLink/CGUpnpService.h>
 #import <CyberLink/CGUpnpAction.h>
 #import <CyberLink/CGUpnpStateVariable.h>
-#else
-#import <CGUpnpControlPoint.h>
-#import <CGUpnpDevice.h>
-#import <CGUpnpService.h>
-#import <CGUpnpAction.h>
-#import <CGUpnpStateVariable.h>
-#endif
+#import <CyberLink/CGUpnpIcon.h>
