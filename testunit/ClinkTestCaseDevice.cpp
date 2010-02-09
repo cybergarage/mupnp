02bad846e83e6fb99b3976877f0bbc29ff8afbab
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2010-02-09 14:52:00 +0000

    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/clinkc/trunk@544 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- testunit/ClinkTestCaseDevice.cpp
+++ testunit/ClinkTestCaseDevice.cpp
@@ -15,5 +15,7 @@ void ClinkTestCase::testDevice()
 	CPPUNIT_ASSERT(testDev);
 	CPPUNIT_ASSERT(strcmp(cg_upnp_device_getfriendlyname(testDev), TEST_DEVICE_FREINDLYNAME) == 0);
 	CPPUNIT_ASSERT(strcmp(cg_upnp_device_getdevicetype(testDev), TEST_DEVICE_DEVICE_TYPE) == 0);
+	CPPUNIT_ASSERT(cg_upnp_device_start(testDev));
+	CPPUNIT_ASSERT(cg_upnp_device_stop(testDev));
 	cg_upnp_device_delete(testDev);
 }
