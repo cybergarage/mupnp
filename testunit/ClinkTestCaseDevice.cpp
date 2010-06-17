e2b3f86f4c2c3439cbfb6bd32173e5dfb0409230
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2010-06-17 12:44:53 +0000

    git-svn-id: https://svn.code.sf.net/p/clinkc/code/trunk@572 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- testunit/ClinkTestCaseDevice.cpp
+++ testunit/ClinkTestCaseDevice.cpp
@@ -15,6 +15,18 @@ void ClinkTestCase::testDevice()
 	CPPUNIT_ASSERT(testDev);
 	CPPUNIT_ASSERT(strcmp(cg_upnp_device_getfriendlyname(testDev), TEST_DEVICE_FREINDLYNAME) == 0);
 	CPPUNIT_ASSERT(strcmp(cg_upnp_device_getdevicetype(testDev), TEST_DEVICE_DEVICE_TYPE) == 0);
+
+	// Embedded Devices
+	int ebdDeviceCnt = 0;
+	for (CgUpnpDevice *ebdDev = cg_upnp_device_getdevices(testDev); ebdDev; ebdDev = cg_upnp_device_next(ebdDev)) {
+		ebdDeviceCnt++;
+		if (ebdDeviceCnt == 1)
+			CPPUNIT_ASSERT(strcmp(cg_upnp_device_getdevicetype(ebdDev), TEST_DEVICE_EMBEDDEDDEVICE_TYPE1) == 0);
+		if (ebdDeviceCnt == 2)
+			CPPUNIT_ASSERT(strcmp(cg_upnp_device_getdevicetype(ebdDev), TEST_DEVICE_EMBEDDEDDEVICE_TYPE2) == 0);
+	}
+	CPPUNIT_ASSERT(ebdDeviceCnt == 2);
+
 	CPPUNIT_ASSERT(cg_upnp_device_start(testDev));
 
 	CPPUNIT_ASSERT(cg_upnp_device_stop(testDev));
