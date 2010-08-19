14d2c838168100797515ba3f559ec69f0d14cc6c
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2010-08-19 17:07:12 +0000

    Added CGNetworkInterface for Objective-C wrapper.
    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/clinkc/trunk@593 bfd3be0d-9f21-0410-925f-e30e9767a5cc

---
+++ wrapper/objc/CyberLink/CGNetworkInterface.m
@@ -0,0 +1,17 @@
+//
+//  CGNetworkInterface.m
+//  iMediaSuite
+//
+//  Created by Satoshi Konno on 10/08/20.
+//  Copyright 2010 Satoshi Konno. All rights reserved.
+//
+
+#import "CGNetworkInterface.h"
+
+@implementation CGNetworkInterface
+
+@synthesize name;
+@synthesize ipaddress;
+@synthesize netmask;
+
+@end
