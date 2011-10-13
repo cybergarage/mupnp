784b54f72dca9a60313254ee93bb9be9f0d90a8d
skonno <skonno@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2011-10-13 13:47:36 +0000

    * Fixed cg_upnp_ssdp_server_thread() and cg_upnp_ssdpresponse_server_thread() to delete the thread using cg_thread_delete().
    git-svn-id: https://clinkc.svn.sourceforge.net/svnroot/clinkc/trunk@669 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- src/cybergarage/upnp/ssdp/cssdp_server.c
+++ src/cybergarage/upnp/ssdp/cssdp_server.c
@@ -165,7 +165,9 @@ static void cg_upnp_ssdp_server_thread(CgThread *thread)
 	
 	cg_upnp_ssdp_packet_delete(ssdpPkt);
 
-	cg_log_debug_l4("Leaving...\n");
+    cg_log_debug_l4("Leaving...\n");
+
+    cg_thread_delete(thread);	
 }
 
 /****************************************
