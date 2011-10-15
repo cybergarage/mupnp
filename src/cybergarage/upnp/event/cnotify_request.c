e745dc206c250977f7427d3c64301e2125d1660e
lundman <lundman@bfd3be0d-9f21-0410-925f-e30e9767a5cc>
2011-10-15 23:42:27 +0000

    This one took quite a few hours to find. In the interest of working with WindowsMediaPlayer12 (win7 etc), it turns out that in the reply to SUBSCRIBE, we must include SERVER: header tag, and in the NOTIFY post, also include SERVER: header. They must also be identical.
    This enables ClinkC to work with WMP12.
    
    I am not sure this is the cleanest way to set the SERVER tag, using 
    cg_http_packet_setheadervalue...
    
    
    
    git-svn-id: https://svn.code.sf.net/p/clinkc/code/trunk@673 bfd3be0d-9f21-0410-925f-e30e9767a5cc

--- src/cybergarage/upnp/event/cnotify_request.c
+++ src/cybergarage/upnp/event/cnotify_request.c
@@ -50,7 +50,7 @@ CgUpnpNotifyRequest *cg_upnp_event_notify_request_new()
 {
 	CgUpnpNotifyRequest *notifyReq;
 	CgUpnpPropertyList *propList;
-	
+
 	cg_log_debug_l4("Entering...\n");
 
 	notifyReq = cg_soap_request_new();
@@ -75,7 +75,7 @@ void cg_upnp_event_notify_request_delete(CgUpnpNotifyRequest *notifyReq)
 
 	propList = cg_upnp_event_notify_request_getpropertylistonly(notifyReq);
 	cg_upnp_propertylist_delete(propList);
-	
+
 	cg_soap_request_delete(notifyReq);
 
 	cg_log_debug_l4("Leaving...\n");
@@ -93,7 +93,7 @@ void cg_upnp_event_notify_request_clear(CgUpnpNotifyRequest *notifyReq)
 
 	propList = cg_upnp_event_notify_request_getpropertylistonly(notifyReq);
 	cg_upnp_propertylist_clear(propList);
-	
+
 	cg_soap_request_clear(notifyReq);
 
 	cg_log_debug_l4("Leaving...\n");
@@ -119,20 +119,25 @@ void cg_upnp_event_notify_request_setsid(CgUpnpNotifyRequest *soapReq, char *sid
 ****************************************/
 BOOL cg_upnp_event_notify_request_setpropertysetnode(CgUpnpNotifyRequest *notifyReq, CgUpnpSubscriber *sub, /* CgUpnpService */void *pservice, CgUpnpStateVariable *statVar)
 {
-	CgHttpRequest *httpReq;	
+	CgHttpRequest *httpReq;
 	CgXmlNode *propSetNode;
 	CgUpnpService* service;
-	
+    char *server[CG_UPNP_SEVERNAME_MAXLEN];
+
 	cg_log_debug_l4("Entering...\n");
 
 	service = (CgUpnpService *)pservice;
-	
+
 	httpReq = cg_soap_request_gethttprequest(notifyReq);
 
 	cg_http_request_setmethod(httpReq, CG_HTTP_NOTIFY);
 	cg_http_request_setconnection(httpReq, CG_HTTP_CLOSE);
 	cg_http_request_seturi(httpReq, cg_upnp_subscriber_getdeliverypath(sub));
 	cg_http_request_sethost(httpReq, cg_upnp_subscriber_getdeliveryhost(sub), cg_upnp_subscriber_getdeliveryport(sub));
+    cg_upnp_getservername(server, sizeof(server));
+    cg_http_packet_setheadervalue((CgHttpPacket*)httpReq,
+                                  CG_HTTP_SERVER,
+                                  server);
 	cg_upnp_event_notify_request_setnt(notifyReq, CG_UPNP_NT_EVENT);
 	cg_upnp_event_notify_request_setnts(notifyReq, CG_UPNP_NTS_PROPCHANGE);
 	cg_upnp_event_notify_request_setsid(notifyReq, cg_upnp_subscriber_getsid(sub));
@@ -156,13 +161,13 @@ static CgXmlNode *cg_upnp_event_notify_request_createpropertysetnode(CgUpnpServi
 	CgXmlNode *propSetNode;
 	CgXmlNode *propNode;
 	CgXmlNode *varNode;
-		
+
 	cg_log_debug_l4("Entering...\n");
 
 	propSetNode = cg_xml_node_new();
 	cg_xml_node_setname(propSetNode, CG_UPNP_NOTIFY_XMLNS CG_SOAP_DELIM CG_UPNP_NOTIFY_PROPERTYSET);
 	cg_xml_node_setnamespace(propSetNode, CG_UPNP_NOTIFY_XMLNS, CG_UPNP_SUBSCRIPTION_XMLNS);
-	
+
 	if (service) {
 		for (statVar = cg_upnp_service_getstatevariables(service); statVar != NULL; statVar = cg_upnp_statevariable_next(statVar)) {
 			if (!cg_upnp_statevariable_issendevents(statVar))
@@ -228,26 +233,26 @@ CgXmlNode *cg_upnp_event_notify_request_getvariablenode(CgUpnpNotifyRequest *nof
 
 	cg_log_debug_l4("Leaving...\n");
 
-	return cg_xml_node_getchildnodes(propNode);		
+	return cg_xml_node_getchildnodes(propNode);
 }
 
 /****************************************
 * cg_upnp_property_createfromnode
 ****************************************/
 
-static CgUpnpProperty *cg_upnp_property_createfromnode(CgXmlNode *varNode) 
+static CgUpnpProperty *cg_upnp_property_createfromnode(CgXmlNode *varNode)
 {
 	CgUpnpProperty *prop;
 	char *varName;
 	char *varValue;
 	int colonIdx;
-	
+
 	cg_log_debug_l4("Entering...\n");
 
 	prop = cg_upnp_property_new();
 	if (varNode == NULL)
 		return prop;
-		
+
 	// remove the event namespace
 	varName = cg_xml_node_getname(varNode);
 	colonIdx = cg_strstr(varName, ":");
@@ -256,7 +261,7 @@ static CgUpnpProperty *cg_upnp_property_createfromnode(CgXmlNode *varNode)
 	varValue = cg_xml_node_getvalue(varNode);
 	cg_upnp_property_setname(prop, varName);
 	cg_upnp_property_setvalue(prop, varValue);
-	
+
 	cg_log_debug_l4("Leaving...\n");
 
 	return prop;
@@ -275,15 +280,15 @@ CgUpnpPropertyList *cg_upnp_event_notify_request_getpropertylist(CgUpnpNotifyReq
 	CgUpnpProperty *prop;
 	char *sid;
 	int seq;
-	
+
 	cg_log_debug_l4("Entering...\n");
 
 	sid = cg_upnp_event_notify_request_getsid(notifyReq);
 	seq = cg_upnp_event_notify_request_getseq(notifyReq);
-		
+
 	propList = cg_upnp_event_notify_request_getpropertylistonly(notifyReq);
 	cg_upnp_propertylist_clear(propList);
-	
+
 	propSetNode = cg_soap_request_getrootnoode(notifyReq);
 	if (propSetNode == NULL)
 		return propList;
@@ -295,7 +300,7 @@ CgUpnpPropertyList *cg_upnp_event_notify_request_getpropertylist(CgUpnpNotifyReq
 		cg_upnp_property_setseq(prop, seq);
 		cg_upnp_propertylist_add(propList, prop);
 	}
-	
+
 	cg_log_debug_l4("Leaving...\n");
 
 	return propList;
