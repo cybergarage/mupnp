383edc23c9d17c892bab9ae9f176f5710d38fa19
Satoshi Konno <skonno@cybergarage.org>
2014-06-24 19:42:18 +0900

    * Added skelton source files for Swift.

---
+++ wrapper/swift/CyberLink/UpnpDevice.swift
@@ -0,0 +1,10 @@
+//
+//  UpnpControlPoint.swift
+//  CyberLink For Swift
+//
+//  Created by Satoshi Konno on 2014/06/24.
+//  Copyright 2014 Satoshi Konno. All rights reserved.
+//
+
+import Foundation
+
