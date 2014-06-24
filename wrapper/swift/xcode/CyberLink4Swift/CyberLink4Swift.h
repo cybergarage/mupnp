8ff5d7bc3118086146752f1b89a2a58b41e70f11
Satoshi Konno <skonno@cybergarage.org>
2014-06-24 19:25:50 +0900

    * Added a first skelton project for Swift.

---
+++ wrapper/swift/xcode/CyberLink4Swift/CyberLink4Swift.h
@@ -0,0 +1,19 @@
+//
+//  CyberLink4Swift.h
+//  CyberLink4Swift
+//
+//  Created by Satoshi Konno on 2014/06/24.
+//  Copyright (c) 2014年 Satoshi Konno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CyberLink4Swift.
+FOUNDATION_EXPORT double CyberLink4SwiftVersionNumber;
+
+//! Project version string for CyberLink4Swift.
+FOUNDATION_EXPORT const unsigned char CyberLink4SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CyberLink4Swift/PublicHeader.h>
+
+
