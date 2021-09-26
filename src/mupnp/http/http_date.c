554d003ace8c683d2518529b5dbfade1adf54812
Satoshi Konno <skonno@cybergarage.org>
2021-09-27 01:21:39 +0900

    Update Update MACOSX_DEPLOYMENT_TARGET from 10.9 to 10.15

--- src/mupnp/http/http_date.c
+++ src/mupnp/http/http_date.c
@@ -13,6 +13,8 @@
 #include "config.h"
 #endif
 
+#include <time.h>
+
 #include <mupnp/http/http.h>
 #include <mupnp/util/log.h>
 #include <mupnp/util/time.h>
@@ -73,6 +75,20 @@ static char* to_week_string(int value)
 /****************************************
 * mupnp_http_getservername()
 ****************************************/
+
+struct tm {
+  int tm_sec;     /* seconds (0 - 60) */
+        int tm_min;     /* minutes (0 - 59) */
+        int tm_hour;    /* hours (0 - 23) */
+        int tm_mday;    /* day of month (1 - 31) */
+        int tm_mon;     /* month of year (0 - 11) */
+        int tm_year;    /* year - 1900 */
+        int tm_wday;    /* day of week (Sunday = 0) */
+        int tm_yday;    /* day of year (0 - 365) */
+        int tm_isdst;   /* is summer time in effect? */
+        char *tm_zone;  /* abbreviation of timezone name */
+        long tm_gmtoff; /* offset from UTC in seconds */
+};
 
 #if !defined(ITRON)
 
