95fbfa21f1698d581c7626992fc0b1a613fd2885
Satoshi Konno <skonno@cybergarage.org>
2021-09-27 01:49:39 +0900

    Remove struct tm for XCode 13

--- src/mupnp/http/http_date.c
+++ src/mupnp/http/http_date.c
@@ -76,20 +76,6 @@ static char* to_week_string(int value)
 * mupnp_http_getservername()
 ****************************************/
 
-struct tm {
-  int tm_sec;     /* seconds (0 - 60) */
-        int tm_min;     /* minutes (0 - 59) */
-        int tm_hour;    /* hours (0 - 23) */
-        int tm_mday;    /* day of month (1 - 31) */
-        int tm_mon;     /* month of year (0 - 11) */
-        int tm_year;    /* year - 1900 */
-        int tm_wday;    /* day of week (Sunday = 0) */
-        int tm_yday;    /* day of year (0 - 365) */
-        int tm_isdst;   /* is summer time in effect? */
-        char *tm_zone;  /* abbreviation of timezone name */
-        long tm_gmtoff; /* offset from UTC in seconds */
-};
-
 #if !defined(ITRON)
 
 const char* mupnp_http_getdate(mUpnpTime sysTime, char* buf, size_t bufSize)
